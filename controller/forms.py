655b8d95b7f64aca94db798b7dba76412efad5e0
dpalominoj <73992058@continental.edu.pe>
2025-07-01 08:56:59 -0500

    Update forms.py

--- controller/forms.py
+++ controller/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, SelectMultipleField, BooleanField
-from wtforms.validators import DataRequired, EqualTo, Length, Optional # Email removido
+from wtforms.validators import DataRequired, EqualTo, Length, Optional
 
 class RegistrationForm(FlaskForm):
     dni = StringField('DNI',
@@ -13,10 +13,7 @@ class RegistrationForm(FlaskForm):
     perfil = SelectField('Perfil',
                        choices=[('voluntario', 'Voluntario'), ('organizador', 'Organizador')],
                        validators=[DataRequired()])
-    # El campo estado_usuario se manejará internamente o por administradores, no se mostrará en el formulario de registro público.
-    # estado_usuario = SelectField('Estado',
-    #                              choices=[('activo', 'Activo'), ('inactivo', 'Inactivo'), ('suspendido', 'Suspendido'), ('bloqueado', 'Bloqueado')],
-    #                              validators=[DataRequired()], default='activo')
+
     discapacidades = SelectMultipleField('Discapacidades', validators=[Optional()])
     preferencias = SelectMultipleField('Preferencias', validators=[Optional()])
     acepto_politica = BooleanField('Acepto la política de privacidad', validators=[DataRequired(message="Debe aceptar la política de privacidad.")])
@@ -31,10 +28,10 @@ class LoginForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired(), Length(min=2, max=100)])
     apellido = StringField('Apellido', validators=[Optional(), Length(max=100)])
-    celular = StringField('Celular', validators=[Optional(), Length(max=9)]) # Ajustar validación si es necesario
-    email = StringField('Email', validators=[Optional(), Length(max=150)]) # Añadir validador de Email si se reinstala email_validator
+    celular = StringField('Celular', validators=[Optional(), Length(max=9)])
+    email = StringField('Email', validators=[Optional(), Length(max=150)])
     direccion = StringField('Dirección', validators=[Optional(), Length(max=255)])
-    fecha_nacimiento = StringField('Fecha de Nacimiento (YYYY-MM-DD)', validators=[Optional()]) # Considerar DateField
+    fecha_nacimiento = StringField('Fecha de Nacimiento (YYYY-MM-DD)', validators=[Optional()])
     genero = SelectField('Género', choices=[('', 'Prefiero no decirlo'), ('masculino', 'Masculino'), ('femenino', 'Femenino')], validators=[Optional()])
     submit = SubmitField('Guardar Cambios')
 
